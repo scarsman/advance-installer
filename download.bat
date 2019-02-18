93babf74889da052476cf62d5d61800bcd2b0b6a
Perpo Hipolito <hperpo@gmail.com>
2019-02-19 00:18:02 +0800

    Update download.bat

--- download.bat
+++ download.bat
@@ -7,8 +7,6 @@ ipfs.exe bootstrap add /ip4/52.32.143.69/tcp/4001/ipfs/QmWSCfwNe3rBtu2on4r7cejHS
 ipfs.exe get /ipfs/QmPvtGa2bU2BPNax6tfgE5qbqePekaXdd4T3KbzpamgZwe -o %temp%\scl-express.msi
 ipfs.exe pin add QmPvtGa2bU2BPNax6tfgE5qbqePekaXdd4T3KbzpamgZwe
 
-msiexec.exe /i %temp%\scl-express.msi /q /L*V %temp%\sclexpress.log
-
 timeout 5
 
 exit
