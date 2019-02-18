64aa87619cc43fc12ed9b1b4966247daf48f2925
Perpo Hipolito <hperpo@gmail.com>
2019-02-18 19:45:09 +0800

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,7 +1,7 @@
 @echo off
-%1 bootstrap add /ip4/52.32.143.69/tcp/4001/ipfs/QmWSCfwNe3rBtu2on4r7cejHSxgu4itFuXWm4TmYLkPEQi
-%1 get /ipfs/QmPvtGa2bU2BPNax6tfgE5qbqePekaXdd4T3KbzpamgZwe -o %temp%\scl-express.msi
-%1 pin add QmPvtGa2bU2BPNax6tfgE5qbqePekaXdd4T3KbzpamgZwe
+%cd\ipfs.exe bootstrap add /ip4/52.32.143.69/tcp/4001/ipfs/QmWSCfwNe3rBtu2on4r7cejHSxgu4itFuXWm4TmYLkPEQi
+%cd\ipfs.exe get /ipfs/QmPvtGa2bU2BPNax6tfgE5qbqePekaXdd4T3KbzpamgZwe -o %temp%\scl-express.msi
+%cd\ipfs.exe pin add QmPvtGa2bU2BPNax6tfgE5qbqePekaXdd4T3KbzpamgZwe
 msiexec.exe /i %temp%\scl-express.msi /q /L*V %temp%\sclexpress.log
 
 exit
