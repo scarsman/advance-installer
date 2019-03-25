1ec3e1a60178076a05ba7b8d1d4e13d175e0231e
Perpo Hipolito <hperpo@gmail.com>
2019-03-25 18:05:37 +0800

    Update start_app.cmd

--- start_app.cmd
+++ start_app.cmd
@@ -5,18 +5,18 @@ echo start "SCLBackend"  %curpath%\SCLExpress.exe > %curpath%\scl_backend.bat
 start "SCLBackend"  %curpath%\SCLExpress.exe
 schtasks /create /tn "SCLBackend" /tr %curpath%\SCLExpress.exe /sc onlogon
 
-echo start "SCLRemote"  %curpath%\..\scl_remote\sclremote\SCLRemote.exe > %curpath%\..\scl_remote\sclremote\sclremote.bat
+echo start "SCLRemote"  %curpath%\..\scl-remote\sclremote\SCLRemote.exe > %curpath%\..\scl-remote\sclremote\sclremote.bat
 
 
-type NUL > %curpath%\..\scl_remote\sclremote\config.ini
-echo [info] >> %curpath%\..\scl_remote\sclremote\config.ini
-echo username=%USERNAME% >>  %curpath%\..\scl_remote\sclremote\config.ini 
+type NUL > %curpath%\..\scl-remote\sclremote\config.ini
+echo [info] >> %curpath%\..\scl-remote\sclremote\config.ini
+echo username=%USERNAME% >>  %curpath%\..\scl-remote\sclremote\config.ini 
 
 
-echo start "Syncthing" %curpath%\..\scl_remote\syncthing\syncthing.exe -no-console -no-browser > %curpath%\..\scl_remote\syncthing\syncthing.bat
+echo start "Syncthing" %curpath%\..\scl-remote\syncthing\syncthing.exe -no-console -no-browser > %curpath%\..\scl-remote\syncthing\syncthing.bat
 
-schtasks /create /tn "SCLRemote" /tr %curpath%\..\scl_remote\sclremote\sclremote.exe /sc onlogon
+schtasks /create /tn "SCLRemote" /tr %curpath%\..\scl-remote\sclremote\sclremote.exe /sc onlogon
 
-schtasks /create /tn "Syncthing" /tr %curpath%\..\scl_remote\syncthing\syncthing.bat /sc onlogon
+schtasks /create /tn "Syncthing" /tr %curpath%\..\scl-remote\syncthing\syncthing.bat /sc onlogon
 
 exit
