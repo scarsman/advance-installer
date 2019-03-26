a50debd8232a9c0327e9ca0d8938dddad62992d8
Perpo Hipolito <hperpo@gmail.com>
2019-03-26 11:39:32 +0800

    Update start_app.cmd

--- start_app.cmd
+++ start_app.cmd
@@ -2,7 +2,7 @@ set curpath="%cd%"
 
 
 start "SCLBackend"  %curpath%\SCLExpress.exe
-schtasks /create /tn "SCLBackend" /tr %curpath%\SCLExpress.exe /sc onlogon
+echo Y | schtasks /create /tn "SCLBackend" /tr %curpath%\SCLExpress.exe /sc onlogon
 
 echo start "SCLRemote"  %curpath%\..\scl-remote\sclremote\SCLRemote.exe > %curpath%\..\scl-remote\sclremote\sclremote.bat
 
@@ -14,8 +14,8 @@ echo username=%USERNAME% >>  %curpath%\..\scl-remote\sclremote\config.ini
 
 echo start "Syncthing" %curpath%\..\scl-remote\syncthing\syncthing.exe -no-console -no-browser > %curpath%\..\scl-remote\syncthing\syncthing.bat
 
-schtasks /create /tn "SCLRemote" /tr %curpath%\..\scl-remote\sclremote\sclremote.exe /sc onlogon
+echo Y | schtasks /create /tn "SCLRemote" /tr %curpath%\..\scl-remote\sclremote\sclremote.exe /sc onlogon
 
-schtasks /create /tn "Syncthing" /tr %curpath%\..\scl-remote\syncthing\syncthing.bat /sc onlogon
+echo Y | schtasks /create /tn "Syncthing" /tr %curpath%\..\scl-remote\syncthing\syncthing.bat /sc onlogon
 
 exit
