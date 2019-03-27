8b01c228b1e54c61fa8516889b4ed438c9027820
Perpo Hipolito <hperpo@gmail.com>
2019-03-27 16:58:40 +0800

    Update start_app.cmd

--- start_app.cmd
+++ start_app.cmd
@@ -1,10 +1,10 @@
 set curpath=%cd%
-if exist %LOCALAPPDATA%\sclexpressdb ( rmdir /s /q %LOCALAPPDATA%\sclexpressdb )
+if exist "%LOCALAPPDATA%\sclexpressdb" rmdir /s /q "%LOCALAPPDATA%\sclexpressdb"
 
-mkdir %LOCALAPPDATA%\sclexpressdb
+mkdir "%LOCALAPPDATA%\sclexpressdb"
 
-echo F | xcopy /S /Q /Y /F %curpath%\scldb8.db %LOCALAPPDATA%\sclexpressdb
-echo F | xcopy /S /Q /Y /F %curpath%\SCL_FTI3.db %LOCALAPPDATA%\sclexpressdb
+echo F | xcopy /S /Q /Y /F "%curpath%\scldb8.db" "%LOCALAPPDATA%\sclexpressdb"
+echo F | xcopy /S /Q /Y /F "%curpath%\SCL_FTI3.db" "%LOCALAPPDATA%\sclexpressdb"
 
 taskkill /F /IM  SCLExpress.exe
 start "SCLBackend"  "%curpath%\SCLExpress.exe"
@@ -14,15 +14,15 @@ schtasks /create /tn "SCLBackend" /tr "%curpath%\sclbackend.bat" /sc onlogon /F
 echo start "SCLRemote"  "%curpath%\..\scl-remote\sclremote\SCLRemote.exe" > %curpath%\..\scl-remote\sclremote\sclremote.bat
 
 
-type NUL > %curpath%\..\scl-remote\sclremote\config.ini
-echo [info] >> %curpath%\..\scl-remote\sclremote\config.ini
-echo username=%USERNAME% >>  %curpath%\..\scl-remote\sclremote\config.ini 
+type NUL > "%curpath%\..\scl-remote\sclremote\config.ini"
+echo [info] >> "%curpath%\..\scl-remote\sclremote\config.ini"
+echo username=%USERNAME% >>  "%curpath%\..\scl-remote\sclremote\config.ini" 
 
 
-echo start "Syncthing" %curpath%\..\scl-remote\syncthing\syncthing.exe -no-console -no-browser > %curpath%\..\scl-remote\syncthing\syncthing.bat
+echo start "Syncthing" "%curpath%\..\scl-remote\syncthing\syncthing.exe -no-console -no-browser" > "%curpath%\..\scl-remote\syncthing\syncthing.bat"
 
 schtasks /create /tn "SCLRemote" /tr "%curpath%\..\scl-remote\sclremote\sclremote.exe" /sc onlogon /F
 
 schtasks /create /tn "Syncthing" /tr "%curpath%\..\scl-remote\syncthing\syncthing.bat" /sc onlogon /F
 
-exit
+pause
