07eb502a226be40fe2f363ad0e47bd184e572331
Perpo Hipolito <hperpo@gmail.com>
2019-04-12 15:12:13 +0800

    Update start_app.cmd

--- start_app.cmd
+++ start_app.cmd
@@ -23,4 +23,3 @@ schtasks /create /tn "SCLRemote" /tr "'%curpath%..\scl-remote\sclremote\sclremot
 
 schtasks /create /tn "Syncthing" /tr "'%curpath%..\scl-remote\syncthing\syncthing.bat'" /sc onlogon /F
 
-exit
